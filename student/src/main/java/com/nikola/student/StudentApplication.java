e14b799d3ad84c18152ece78b8fb2eaeac9616c5
smilynik <nikola_smilyanski@flutterint.com>
2023-05-24 21:50:06 +0300

    init commit

---
+++ student/src/main/java/com/nikola/student/StudentApplication.java
@@ -0,0 +1,13 @@
+package com.nikola.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//19:40 video
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+
+}
